ad4f1777b1ee258dc2a08c65ca9cf97a256c5f56
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2019-06-09 17:38:13 +0100

    Added edit button

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ class UI {
             <td>${book.title}</td>
             <td>${book.author}</td>
             <td>${book.isbn}</td>
-            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a>
+            <a href="#" class="btn btn-danger btn-sm edit">Y</a></td>
         `;
 
         list.appendChild(row);
